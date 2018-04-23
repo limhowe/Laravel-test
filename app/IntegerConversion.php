064ea0ce4f295dad80136df25f86ebfaa71daadf
Lim Howe <limhowe1214@gmail.com>
2018-04-23 09:16:58 -0500

    ADD: Arabic to Roman, pass test

--- app/IntegerConversion.php
+++ app/IntegerConversion.php
@@ -2,7 +2,17 @@
 
 namespace App;
 
-class IntegerConversion
+class IntegerConversion implements IntegerConversionInterface
 {
+    private $numeral_key = array("M"=>1000, "CM"=>900, "D"=>500, "CD"=>400, "C"=>100, "XC"=>90, "L"=>50, "XL"=>40, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1);
 
+    public function toRomanNumerals($integer)
+    {
+        $string = "";
+        foreach ($this->numeral_key as $roman_number=>$arabic_number) {
+            $string .= str_repeat($roman_number, intval($integer / $arabic_number));
+            $integer = $integer % $arabic_number;
+        }
+        return $string;
+    }
 }
